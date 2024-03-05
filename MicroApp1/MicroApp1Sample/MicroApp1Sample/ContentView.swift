875e45d9a2aa8051cebf3ea3a6ccbe1b7505e5b4
Andrew Brehm <andrew.brehm@dialexa.com>
2024-03-05 17:19:57 -0500

    initial commit

---
+++ MicroApp1/MicroApp1Sample/MicroApp1Sample/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  MicroApp1Sample
+//
+//  Created by Andrew Brehm on 3/5/24.
+//
+
+import MicroApp1
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MicroApp1NavigationView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MicroApp1/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MicroApp1",
+    platforms: [.iOS(.v17)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MicroApp1",
+            targets: ["MicroApp1"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AndrewBrehmAtDialexa/Navigation", from: "1.0.0"),
+        .package(url: "https://github.com/AndrewBrehmAtDialexa/CommonUI", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MicroApp1",
+            dependencies: [
+                .product(name: "Navigation", package: "Navigation"),
+                .product(name: "CommonUI", package: "CommonUI"),
+            ]),
+        .testTarget(
+            name: "MicroApp1Tests",
+            dependencies: ["MicroApp1"]),
+    ])
