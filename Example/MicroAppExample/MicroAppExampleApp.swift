58e5a809b8baf351464314740cfbc697a25c3153
Andrew Brehm <andrew.brehm@dialexa.com>
2024-03-05 19:01:14 -0500

    microapp now working

---
+++ Example/MicroAppExample/MicroAppExampleApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import MicroApp1
+
+@main
+struct MicroAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MicroApp1NavigationView()
+        }
+    }
+}
